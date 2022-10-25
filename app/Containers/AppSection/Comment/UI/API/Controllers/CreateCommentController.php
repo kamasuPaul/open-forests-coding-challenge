d7a243ac936a175fee8623325f344fbf82f05bf3
Kamasu Paul <kamasupaul@gmail.com>
2022-10-26 02:51:46 +0300

    -- create comment action

--- app/Containers/AppSection/Comment/UI/API/Controllers/CreateCommentController.php
+++ app/Containers/AppSection/Comment/UI/API/Controllers/CreateCommentController.php
@@ -2,9 +2,16 @@
 
 namespace App\Containers\AppSection\Comment\UI\API\Controllers;
 
+use App\Containers\AppSection\Comment\Actions\CreateCommentAction;
+use App\Containers\AppSection\Comment\UI\API\Requests\CreateCommentRequest;
+use App\Containers\AppSection\Comment\UI\API\Transformers\CommentTransformer;
 use App\Ship\Parents\Controllers\ApiController;
 
 class CreateCommentController extends ApiController
 {
-    //
+    public function createComment(CreateCommentRequest $request)
+    {
+        $comment = app(CreateCommentAction::class)->run($request);
+        return $this->transform($comment, CommentTransformer::class);
+    }
 }
---
+++ app/Containers/AppSection/Comment/UI/API/Requests/CreateCommentRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Containers\AppSection\Comment\UI\API\Requests;
+
+use App\Ship\Parents\Requests\Request as ParentRequest;
+
+class CreateCommentRequest extends ParentRequest
+{
+    /**
+     * Define which Roles and/or Permissions has access to this request.
+     */
+    protected array $access = [
+        'permissions' => '',
+        'roles' => '',
+    ];
+
+    /**
+     * Id's that needs decoding before applying the validation rules.
+     */
+    protected array $decode = [
+        // 'id',
+    ];
+
+    /**
+     * Defining the URL parameters (e.g, `/user/{id}`) allows applying
+     * validation rules on them and allows accessing them like request data.
+     */
+    protected array $urlParameters = [
+        'post_id'
+    ];
+
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|min:3|max:50',
+            'comment' => 'required|string|min:3|max:2500',
+            'post_id' => 'required|exists:posts,id',
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->check([
+            'hasAccess',
+        ]);
+    }
+}
